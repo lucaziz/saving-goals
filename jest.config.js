ea298c8e4325d9b6739d62d3f848a6f10303c281
Lucas Aziz <lucaziz@gmail.com>
2020-03-03 22:59:05 -0300

    [LA] Configure tests and adds helpers

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,19 @@ module.exports = {
     '^.+\\.jsx?$': 'babel-jest',
     '^.+\\.tsx?$': 'ts-jest'
   },
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.jest.json',
+      diagnostics: false
+    }
+  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   testPathIgnorePatterns: ['/node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true,
   moduleNameMapper: {
-    '\\.(svg|jpg|png|css)$': '<rootDir>/spec/empty-module.js'
+    '\\.(svg|jpg|png|css)$': '<rootDir>/spec/empty-module.js',
+    '~/(.*)$': '<rootDir>/src/$1'
   },
   setupFilesAfterEnv: ['<rootDir>spec/setup.js'],
   moduleDirectories: ['node_modules', 'src']
---
+++ src/utils/testHelpers.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { theme } from '~styles/theme/theme';
+import { ThemeProvider } from 'styled-components';
+import { mount, shallow } from 'enzyme';
+
+export const buildSpy = () => jest.fn();
+
+export const simulateTyping = (element: any, value: string) =>
+  element.simulate('change', { target: { value } });
+
+export function mountWithTheme(child: any) {
+  return mount(child, {
+    wrappingComponent: ({ children }) => (
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    )
+  });
+}
+
+export function shallowWithTheme(child: any) {
+  return shallow(child, {
+    wrappingComponent: ({ children }) => (
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    )
+  });
+}
